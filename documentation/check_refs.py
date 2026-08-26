aeab6a6f48d91562219f1b857cae00b70a10b50c
James Sargant <shorinbonsai@gmail.com>
2026-08-26 17:30:56 -0400

    check_refs: MOD's new-genome.html entry pointed at the deleted mod.rs
    Repointed to genomes.rs, matching the parity already done for new-evolver.html's entry.

--- documentation/check_refs.py
+++ documentation/check_refs.py
@@ -26,7 +26,7 @@ import re
 import sys
 
 MOD = {
-    "documentation/guide/new-genome.html": "get/src/genomes/mod.rs",
+    "documentation/guide/new-genome.html": "get/src/genomes.rs",
     "documentation/guide/new-evolver.html": "get/src/evolver.rs",
 }
 CHAIN = {
